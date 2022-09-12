26450e678073c6e7fa4ad49be19bdf908643187f
PAU COLOME <pau.colome@vodafone.com>
2022-09-12 12:52:35 +0200

    usereducer theme

--- src/header.jsx
+++ src/header.jsx
@@ -2,14 +2,18 @@ import './header.css'
 import { useTheme } from './theme-context'
 
 export default function Header() {
-    const { isLight, toggleTheme } = useTheme()
+	const { isLight, dispatch } = useTheme()
+
+	const handleToggle = () => {
+		dispatch({ type: 'toggle' })
+	}
 
 	return (
 		<header className="header flex justify-space-between">
 			<h1>TODO</h1>
-            <button className="btn" onClick={toggleTheme}>
-                {isLight ? '🌙' : '☀️'}
-            </button>
+			<button className="btn" onClick={handleToggle}>
+				{isLight ? '🌙' : '☀️'}
+			</button>
 		</header>
 	)
 }
--- src/theme-context.jsx
+++ src/theme-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useReducer } from 'react'
 
 export const THEMES = Object.freeze({
 	LIGHT: 'light',
@@ -8,25 +8,34 @@ export const THEMES = Object.freeze({
 const ThemeContext = createContext({
 	theme: THEMES.DARK,
 	isLight: false,
-	toggleTheme: () => {},
+	dispatch: () => {},
 })
 
 export function useTheme() {
 	return useContext(ThemeContext)
 }
 
-export function ThemeProvider({ children }) {
-	const [theme, setTheme] = useState(THEMES.DARK)
-
-	const toggleTheme = () => {
-		setTheme((theme) =>
-			theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT
-		)
+function reducer(state, action) {
+	switch (action.type) {
+		case 'toggle':
+			return {
+				...state,
+				theme:
+					state.theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT,
+			}
+		default:
+			return state
 	}
+}
+
+const initialState = { theme: THEMES.DARK }
+
+export function ThemeProvider({ children }) {
+	const [state, dispatch] = useReducer(reducer, initialState)
 
 	return (
 		<ThemeContext.Provider
-			value={{ theme, isLight: theme === THEMES.LIGHT, toggleTheme }}
+			value={{ state, isLight: state.theme === THEMES.LIGHT, dispatch }}
 		>
 			{children}
 		</ThemeContext.Provider>
